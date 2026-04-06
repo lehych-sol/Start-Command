4acb521e37fa54e01c7222531dae182c6e8cdc1b
lehych <lehych.sol@gmail.com>
2026-04-06 16:28:56 +0300

    Update nsfwzimage.py

--- nsfwzimage.py
+++ nsfwzimage.py
@@ -4,18 +4,20 @@ import subprocess
 base = "/workspace/ComfyUI/models"
 
 downloads = [
-    ("text_encoders", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/text_encoders/qwen.safetensors"),
-    ("checkpoints", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/checkpoints/SDXLNSFW.safetensors"),
-    ("vae", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/vae/variational_encoder_primary.safetensors"),
-    ("diffusion_models", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/diffusion_models/zimage.safetensors"),
-    ("ultralytics", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/bbox/lips_v1.pt"),
-    ("ultralytics", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/bbox/nipple.pt"),
-    ("ultralytics", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/bbox/pussyV2.pt"),
-    ("upscale_models", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/upscale_models/4x-UltraSharpV2.pth"),
-    ("upscale_models", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/upscale_models/4x_NMKD-Superscale-SP_178000_G.pth"),
-    ("upscale_models", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/upscale_models/x1_ITF_SkinDiffDetail_Lite_v1.pth"),
-    ("loras", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/loras/DetailedNipples.safetensors"),
-    ("loras", "https://huggingface.co/lehychh/Z-ImageTurbo-SDXL-NSFW/resolve/main/loras/dmd2_sdxl_4step_lora_fp16.safetensors"),
+    ("ultralytics/bbox", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/bbox/face_yolov8m.pt"),
+    ("ultralytics/bbox", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/bbox/hand_yolov8s.pt"),
+    ("ultralytics/bbox", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/bbox/lips_v1.pt"),
+    ("ultralytics/bbox", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/bbox/nipple.pt"),
+    ("ultralytics/bbox", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/bbox/pussyV2.pt"),
+    ("checkpoints", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/checkpoints/SDXLNSFW.safetensors"),
+    ("text_encoders", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/clip/qwen.safetensors"),
+    ("diffusion_models", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/diffusion_models/zimage.safetensors"),
+    ("loras", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/loras/DetailedNipples.safetensors"),
+    ("loras", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/loras/dmd2_sdxl_4step_lora_fp16.safetensors"),
+    ("upscale_models", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/upscale_models/4x-UltraSharpV2.pth"),
+    ("upscale_models", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/upscale_models/4x_NMKD-Superscale-SP_178000_G.pth"),
+    ("upscale_models", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/upscale_models/x1_ITF_SkinDiffDetail_Lite_v1.pth"),
+    ("vae", "https://huggingface.co/lehychh/Z-image-SDXL-t2i/resolve/main/vae/ae.safetensors"),
 ]
 
 for folder, url in downloads:
